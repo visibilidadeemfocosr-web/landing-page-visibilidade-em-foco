186b1d3e0f2c753b0d5fcd5e3d1e753d5e1465e2
visibilidadeemfocosr-web <visibilidade.emfocosr@gmail.com>
2025-12-08 15:04:37 -0300

    fix: restore radio button functionality to match production commit ceff86f
    - Restored border-color with !important in globals.css
    - Restored RadioGroupItem to use CircleIcon from lucide-react (as in ceff86f)
    - Kept touch-action: manipulation on all inputs/buttons (as in ceff86f)
    - Maintained touch-manipulation class on radio button labels
    - Radio buttons now work correctly in both home and preview-new-2

--- components/ui/radio-group.tsx
+++ components/ui/radio-group.tsx
@@ -2,6 +2,7 @@
 
 import * as React from 'react'
 import * as RadioGroupPrimitive from '@radix-ui/react-radio-group'
+import { CircleIcon } from 'lucide-react'
 
 import { cn } from '@/lib/utils'
 
@@ -35,7 +36,7 @@ function RadioGroupItem({
         data-slot="radio-group-indicator"
         className="relative flex items-center justify-center"
       >
-        <div className="absolute top-1/2 left-1/2 size-2 -translate-x-1/2 -translate-y-1/2 rounded-full bg-primary" />
+        <CircleIcon className="fill-primary absolute top-1/2 left-1/2 size-2 -translate-x-1/2 -translate-y-1/2" />
       </RadioGroupPrimitive.Indicator>
     </RadioGroupPrimitive.Item>
   )
